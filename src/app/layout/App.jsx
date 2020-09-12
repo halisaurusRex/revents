940ae28990b3c27efbddff6aa7c72eb3f76c4723
halisaurusRex <jknightly618@hotmail.com>
2020-09-12 13:55:12 -0700

    End of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -5,14 +5,30 @@ import { Container } from 'semantic-ui-react';
 
 export default function App() {
   const [formOpen, setFormOpen]= useState(false);
+  const [selectedEvent, setSelectedEvent]= useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
 
   return (
 
     <>
       <h1>Re-vents</h1>
-      <NavBar setFormOpen={setFormOpen} />
+      <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className='main'>
-        <EventDashboard setFormOpen={setFormOpen} formOpen={formOpen}/>
+        <EventDashboard 
+          setFormOpen={setFormOpen} 
+          formOpen={formOpen} 
+          selectEvent={handleSelectEvent} 
+          selectedEvent={selectedEvent}  
+        />
       </Container>
     </>
   );
